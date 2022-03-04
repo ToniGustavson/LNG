172ceb8c0de5bb97970037109df4d696cf793671
t.busch <t.busch@fz-juelich.de>
2022-03-04 15:36:10 +0100

    Added Plotly plots

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,158 +1,79 @@
+#%%
 import altair as alt
 import pandas as pd
 import streamlit as st
-from vega_datasets import data
+import matplotlib.pyplot as plt
+import numpy as np
+import plotly.express as px
+import plotly.graph_objects as go
+import plotly.figure_factory as ff
+
+#%%
+FZJcolor = pd.read_csv("Input/FZJcolor.csv")
+
+def rgb_to_hex(reg_vals):
+    def clamp(x): 
+        return int(max(0, min(x*255, 255)))
+    return "#{0:02x}{1:02x}{2:02x}".format(clamp(reg_vals[0]), clamp(reg_vals[1]), clamp(reg_vals[2]))
 
 st.set_page_config(
-    page_title="Time series annotations", page_icon="⬇", layout="centered"
+    page_title="LNG", page_icon="🚢", layout="wide" # layout="wide"
 )
 
+st.title("(In)Dependence from Russian Natural Gas")
 
-@st.experimental_memo
-def get_data():
-    source = data.stocks()
-    source = source[source.date.gt("2004-01-01")]
-    return source
+# Reading in Data
+df = pd.read_excel("Input/lng_data.xlsx")
+
+# Data manipulation
+df["dtmi_median"] = df["dtmi"].median()
+df["free_inventory"] = df["dtmi_median"] - df["lngInventory"]
+xval = df["gasDayStartedOn"]
 
 
-@st.experimental_memo(ttl=60 * 60 * 24)
-def get_chart(data):
-    hover = alt.selection_single(
-        fields=["date"],
-        nearest=True,
-        on="mouseover",
-        empty="none",
+# st.markdown("## Pyplot - Level of LNG storage facilities in the EU")
+# fig, ax = plt.subplots()
+# ax.plot(xval, df["lngInventory"], label="State of charge", color = FZJcolor.blue) #range(len(df))
+# ax.plot(xval, df["dtmi_median"],  label="Capacity", color = FZJcolor.orange)
+# ax.legend()
+# ax.set_ylim(ymin=0, ymax=8000)
+# ax.set_ylabel("LNG [10$^3$ m$^3$]")
+# ax.set_xlabel("Date")
+# ax.grid(visible=True)
+# st.pyplot(fig)
+
+
+inventory = go.Bar(x=xval, y=df["lngInventory"], name="State of charge", marker=dict(color= rgb_to_hex(FZJcolor.blue)))
+capacity_max = go.Line(x=xval, y=df["dtmi_median"], name = "Capacity",  marker=dict(color= rgb_to_hex(FZJcolor.orange)))
+
+fig = go.Figure()
+fig.add_trace(inventory)
+fig.add_trace( capacity_max)
+
+fig.update_layout(
+    title="Storage level of LNG facilities in the EU",
+    yaxis_title= "LNG [10^3 m^3]",
+    yaxis=dict(range=[0, 8000]),
+    font=dict(
+        size=18,
     )
-
-    lines = (
-        alt.Chart(data, title="Evolution of stock prices")
-        .mark_line()
-        .encode(
-            x="date",
-            y="price",
-            color="symbol",
-            # strokeDash="symbol",
-        )
-    )
-
-    # Draw points on the line, and highlight based on selection
-    points = lines.transform_filter(hover).mark_circle(size=65)
-
-    # Draw a rule at the location of the selection
-    tooltips = (
-        alt.Chart(data)
-        .mark_rule()
-        .encode(
-            x="yearmonthdate(date)",
-            y="price",
-            opacity=alt.condition(hover, alt.value(0.3), alt.value(0)),
-            tooltip=[
-                alt.Tooltip("date", title="Date"),
-                alt.Tooltip("price", title="Price (USD)"),
-            ],
-        )
-        .add_selection(hover)
-    )
-
-    return (lines + points + tooltips).interactive()
-
-
-st.title("⬇ Time series annotations")
-
-st.write("Give more context to your time series using annotations!")
-
-col1, col2, col3 = st.columns(3)
-with col1:
-    ticker = st.text_input("Choose a ticker (⬇💬👇ℹ️ ...)", value="⬇")
-with col2:
-    ticker_dx = st.slider(
-        "Horizontal offset", min_value=-30, max_value=30, step=1, value=0
-    )
-with col3:
-    ticker_dy = st.slider(
-        "Vertical offset", min_value=-30, max_value=30, step=1, value=-10
-    )
-
-# Original time series chart. Omitted `get_chart` for clarity
-source = get_data()
-chart = get_chart(source)
-
-# Input annotations
-ANNOTATIONS = [
-    ("Mar 01, 2008", "Pretty good day for GOOG"),
-    ("Dec 01, 2007", "Something's going wrong for GOOG & AAPL"),
-    ("Nov 01, 2008", "Market starts again thanks to..."),
-    ("Dec 01, 2009", "Small crash for GOOG after..."),
-]
-
-# Create a chart with annotations
-annotations_df = pd.DataFrame(ANNOTATIONS, columns=["date", "event"])
-annotations_df.date = pd.to_datetime(annotations_df.date)
-annotations_df["y"] = 0
-annotation_layer = (
-    alt.Chart(annotations_df)
-    .mark_text(size=15, text=ticker, dx=ticker_dx, dy=ticker_dy, align="center")
-    .encode(
-        x="date:T",
-        y=alt.Y("y:Q"),
-        tooltip=["event"],
-    )
-    .interactive()
 )
+# Plot!
+st.plotly_chart(fig, use_container_width=True)
 
-# Display both charts together
-st.altair_chart((chart + annotation_layer).interactive(), use_container_width=True)
 
-st.write("## Code")
+### Free Capacity
+free_cap = go.Bar(x=xval, y=df["free_inventory"], name="State of charge", marker=dict(color= rgb_to_hex(FZJcolor.green)))
+fig = go.Figure()
+fig.add_trace(free_cap)
 
-st.write(
-    "See more in our public [GitHub repository](https://github.com/streamlit/example-app-time-series-annotation)"
-)
-
-st.code(
-    f"""
-import altair as alt
-import pandas as pd
-import streamlit as st
-from vega_datasets import data
-
-@st.experimental_memo
-def get_data():
-    source = data.stocks()
-    source = source[source.date.gt("2004-01-01")]
-    return source
-
-source = get_data()
-
-# Original time series chart. Omitted `get_chart` for clarity
-chart = get_chart(source)
-
-# Input annotations
-ANNOTATIONS = [
-    ("Mar 01, 2008", "Pretty good day for GOOG"),
-    ("Dec 01, 2007", "Something's going wrong for GOOG & AAPL"),
-    ("Nov 01, 2008", "Market starts again thanks to..."),
-    ("Dec 01, 2009", "Small crash for GOOG after..."),
-]
-
-# Create a chart with annotations
-annotations_df = pd.DataFrame(ANNOTATIONS, columns=["date", "event"])
-annotations_df.date = pd.to_datetime(annotations_df.date)
-annotations_df["y"] = 0
-annotation_layer = (
-    alt.Chart(annotations_df)
-    .mark_text(size=15, text="{ticker}", dx={ticker_dx}, dy={ticker_dy}, align="center")
-    .encode(
-        x="date:T",
-        y=alt.Y("y:Q"),
-        tooltip=["event"],
+fig.update_layout(
+    title="Spare capacity (Capacity - State of charge)",
+    yaxis_title= "LNG [10^3 m^3]",
+    yaxis=dict(range=[0, 8000]),
+    font=dict(
+        size=18,
     )
-    .interactive()
 )
-
-# Display both charts together
-st.altair_chart((chart + annotation_layer).interactive(), use_container_width=True)
-
-""",
-    "python",
-)
+# Plot!
+st.plotly_chart(fig, use_container_width=True)
