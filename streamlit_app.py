86564c551122344d55b72dbfac179bfac20f7666
t.busch <t.busch@fz-juelich.de>
2022-03-08 08:47:08 +0100

    automated commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,6 +34,8 @@ def get_color(key, default_col="blue"):
     return {"Russia": FZJcolor.get(default_col)}.get(key, FZJcolor.get("grey1") )
 
 
+legend_dict = dict(orientation="h", yanchor="top", y=-0.1, xanchor="center", x=0.5)
+font_dict = dict(size=18)
 
 font_size = 18
 
@@ -55,30 +57,38 @@ st.markdown("# Energy imports from Russia and possible alternatives")
 st.markdown("## EU energy imports by country of origin")
 
 cols = st.columns(3)
+
 # Natrural Gas Import
-cols[0].markdown("### Natural gas")
-# cols[0].markdown("[10$^3$ TWh/a]")
 colors = [get_color(x, default_col="blue4") for x in ng_share.index]
 fig = go.Figure()
 fig.add_trace(go.Pie(labels=ng_share.index, values = ng_share.value, hole=.3, marker=dict(colors=colors)))
+fig.update_layout(
+    title="Natural gas",
+    font=dict(size=16),
+)
+
 cols[0].plotly_chart(fig, use_container_width=True)
 cols[0].caption("Source: Eurostat, 2020")
 
 # Solid Fuels
-cols[1].markdown("### Solid fuels")
 colors = [get_color(x, default_col="black") for x in solid_fuel_share.index]
 fig = go.Figure()
 fig.add_trace(go.Pie(labels=solid_fuel_share.index, values = solid_fuel_share.value, hole=.3, marker=dict(colors=colors)))
-
+fig.update_layout(
+    title="Solid fuels",
+    font=dict(size=16),
+)
 cols[1].plotly_chart(fig, use_container_width=True)
 cols[1].caption("Source: Eurostat, 2019")
 
 # Crude oil imports
-cols[2].markdown("### Crude oil")
 colors = [get_color(x, default_col="black3") for x in crude_oil_share.index]
 fig = go.Figure()
 fig.add_trace(go.Pie(labels=crude_oil_share.index, values = crude_oil_share.value, hole=.3, marker=dict(colors=colors)))
-
+fig.update_layout(
+    title="Crude oil",
+    font=dict(size=16),
+)
 cols[2].plotly_chart(fig, use_container_width=True)
 cols[2].caption("Source: Eurostat, 2019")
 
@@ -94,9 +104,8 @@ fig.update_layout(
     title="EU pipeline imports from Russia",
     yaxis_title= "NG [GWh/d]",
     yaxis=dict(range=[0, 2700]),
-    font=dict(
-        size=font_size,
-    ),
+    font=font_dict,
+    legend=legend_dict,
     barmode='stack',
 )
 
@@ -126,9 +135,8 @@ fig.update_layout(
     title="Storage level of LNG facilities in the EU",
     yaxis_title= "LNG [TWh]",
     yaxis=dict(range=[0, 60]),
-    font=dict(
-        size=font_size,
-    )
+    font=font_dict,
+    legend=legend_dict,
 )
 col1.plotly_chart(fig, use_container_width=True)
 col1.caption("Source: GIE, 2022")
@@ -143,9 +151,8 @@ fig.update_layout(
     title="Spare LNG storage capacity (Max capacity - State of charge)",
     yaxis_title= "LNG [TWh]",
     yaxis=dict(range=[0, 60]),
-    font=dict(
-        size=font_size,
-    )
+    font=font_dict,
+    legend=legend_dict,
 )
 col1.plotly_chart(fig, use_container_width=True)
 col1.caption("Source: GIE, 2022")
@@ -161,9 +168,8 @@ fig.update_layout(
     title="Send out of LNG",
     yaxis_title= "LNG [GWh/d]",
     yaxis=dict(range=[0, 7000]),
-    font=dict(
-        size=font_size,
-    )
+    font=font_dict,
+    legend=legend_dict,
 )
 col1.plotly_chart(fig, use_container_width=True)
 col1.caption("Source: GIE, 2022")
@@ -182,9 +188,8 @@ fig.update_layout(
     title="Storage level NG in the EU",
     yaxis_title= "NG [TWh]",
     yaxis=dict(range=[0, 1200]),
-    font=dict(
-        size=font_size,
-    )
+    font=font_dict,
+    legend=legend_dict,
 )
 
 col2.plotly_chart(fig, use_container_width=True)
@@ -201,9 +206,8 @@ fig.update_layout(
     title="Spare NG storage capacity (Max capacity - State of charge)",
     yaxis_title= "NG [TWh]",
     yaxis=dict(range=[0, 1200]),
-    font=dict(
-        size=font_size,
-    )
+    font=font_dict,
+    legend=legend_dict,
 )
 col2.plotly_chart(fig, use_container_width=True)
 col2.caption("Source: GIE, 2022")
@@ -218,9 +222,8 @@ fig.update_layout(
     title="Withdrawal of NG",
     yaxis_title= "NG [GWh/d]",
     yaxis=dict(range=[0, 20000]),
-    font=dict(
-        size=font_size,
-    )
+    font=font_dict,
+    legend=legend_dict,
 )
 col2.plotly_chart(fig, use_container_width=True)
 col2.caption("Source: GIE, 2022")
